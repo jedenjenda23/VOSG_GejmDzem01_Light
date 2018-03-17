a8a2349a2ae7cc17b59d8e3c89ef07679ec4b9ad
Jan Fidler <honzaf28@gmail.com>
2018-03-17 14:16:37 +0100

    asdfsdgdsfgfdsgfdsgfbdfdjfgsdfdgfhjgfzdfhjgfhdhjgxhfdxghxfdgncgfdgxffbnxcvfnvxchfdgfghjfdhjgdf, jep I'm cool

---
+++ Assets/Scripts/AI_Firefly.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+
+public class AI_Firefly : MonoBehaviour
+{
+    public float movementForce = 700f;
+    Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<PlayerController>())
+        {
+            Vector3 direction = transform.position - other.transform.position;
+            Dash(direction);
+        }
+    }
+
+    void Dash(Vector3 dir)
+    {
+
+        dir.x -= Random.RandomRange(0, 4f);
+        dir.x += Random.RandomRange(0, 4f);
+
+        rb.AddForce(dir.normalized * Random.Range(0.5f * movementForce, 1.25f * movementForce), ForceMode.Impulse);
+    }
+}
