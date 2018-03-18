f14af705dadab9689c90f40c4b1149a0c40c13f5
Jan Fidler <honzaf28@gmail.com>
2018-03-18 14:05:49 +0100

    audio player system

---
+++ Assets/Scripts/Player/PlayerAudio.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAudio : MonoBehaviour
+{
+    PlayerController playerController;
+
+    public AudioClip[] softSteps;
+    public AudioClip[] hardSteps;
+    public AudioClip[] woodSteps;
+    public AudioClip[] mudSteps;
+
+    public AudioSource audioSource;
+
+    private void Awake()
+    {
+        playerController = GetComponentInParent<PlayerController>();
+    }
+
+    public void PlayStep()
+    {
+        switch(playerController.currentSurface)
+        {
+            case surfaceType.Ground:
+                PlayRandomFromArray(softSteps);
+                break;
+        }
+    }
+
+
+    public void PlayRandomFromArray(AudioClip[] myArray)
+    {
+        int index = Random.Range(0, myArray.Length - 1);
+
+        audioSource.PlayOneShot(myArray[index]);
+    }
+}
