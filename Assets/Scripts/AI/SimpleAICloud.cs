bde1635f3616a2d507d8168a676a29c95633e6b1
Jan Fidler <honzaf28@gmail.com>
2018-04-18 21:29:14 +0200

    enemy fog, waypoints

---
+++ Assets/Scripts/AI/SimpleAICloud.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleAICloud : MonoBehaviour
+{
+    public enum states { Idle, Patrol}
+    public states myState = states.Idle;
+
+    public Transform[] waypoints;
+
+    [Range(0, 30)]
+    public float travelSpeed = 1f;
+
+    int myWaypoint = 0;
+    float waitingTimer = 0;
+
+    private void FixedUpdate()
+    {
+        StateMachineUpdate();
+    }
+
+    void StateMachineUpdate()
+    {
+        switch(myState)
+        {
+            case states.Idle:
+                break;
+
+            case states.Patrol:
+                ActionPatrol();
+                break;
+        }
+    }
+
+    void ActionPatrol()
+    {
+        if (Vector3.Distance(transform.position, waypoints[myWaypoint].position) < 0.1f)
+        {
+            if (waypoints[myWaypoint].GetComponent<WaypointClass>() != null)
+            {
+                if (waypoints[myWaypoint].GetComponent<WaypointClass>().wpAction == actions.Destroy)
+                {
+                    Destroy(gameObject);
+                }
+
+                else
+                {
+                    waitingTimer += 1 * Time.deltaTime;
+
+                    if (waitingTimer > waypoints[myWaypoint].GetComponent<WaypointClass>().waitingTime)
+                    {
+                        waitingTimer = 0;
+                        myWaypoint++;
+                    }
+                }
+            }
+            else
+            {
+                waitingTimer = 0;
+                myWaypoint++;
+            }
+
+        }
+
+        /*
+        if (Vector3.Distance(transform.position, waypoints[myWaypoint].position) > 0.5f)
+        {
+            // Calculate direction from character to mouse position
+            Vector3 direction = waypoints[myWaypoint].position - transform.position;
+
+            // Direction to rotation
+            Quaternion targetRotation = Quaternion.LookRotation(direction, transform.position);
+            targetRotation.x = 0;
+            targetRotation.z = 0;
+            // Spherical interpolation to desired rotation
+            transform.rotation = Quaternion.SlerpUnclamped(transform.rotation, targetRotation, 10f * Time.deltaTime);
+        }
+        */
+
+        if (myWaypoint > waypoints.Length - 1) myWaypoint = 0;
+        // transform.position = Vector3.Lerp(transform.position, waypoints[myWaypoint].position, travelSpeed * Time.deltaTime);
+
+        transform.LookAt(waypoints[myWaypoint]);
+        transform.Translate(Vector3.forward * travelSpeed * 0.01f);
+    }
+
+
+#if UNITY_EDITOR
+    private void OnDrawGizmos()
+    {
+        for(int i = 1; i < waypoints.Length; i++)
+        {
+            Debug.DrawLine(waypoints[i].position, waypoints[i - 1].position, Color.green);
+        }
+
+        Debug.DrawLine(waypoints[0].position, waypoints[waypoints.Length - 1].position, Color.green);
+    }
+#endif 
+}
---
+++ Assets/Scripts/AI/WaypointClass.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum actions { None, Destroy }
+
+public class WaypointClass : MonoBehaviour
+{
+
+    public actions wpAction = actions.None;
+
+    [Range(0, 20)]
+    public float waitingTime = 0f;
+
+#if UNITY_EDITOR
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.white;
+
+        if(wpAction == actions.Destroy)
+        {
+            UnityEditor.Handles.Label(transform.position, "Destroy");
+            Gizmos.color = Color.red;
+        }
+
+
+        else if(wpAction == actions.None)
+        {
+            if (waitingTime > 0)
+            {
+                UnityEditor.Handles.Label(transform.position, "Wait: " + waitingTime);
+            }
+            Gizmos.color = Color.yellow;
+        }
+
+        Gizmos.DrawSphere(transform.position, 0.35f);
+    }
+#endif 
+}
