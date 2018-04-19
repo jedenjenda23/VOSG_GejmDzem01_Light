fd05398a0a841de64c95205cb7b1eeb5ab39a48c
Jan Fidler <honzaf28@gmail.com>
2018-04-19 14:55:10 +0200

    playerui - static + dontDestroyOnLoad, load scene trigger

---
+++ Assets/Scripts/LoadSceneTrigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadSceneTrigger : MonoBehaviour
+{
+
+    public string targetScene = "Onion Knight forever...";
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<PlayerController>())
+        {
+            MenuFunctions.instance.LoadScene(targetScene);
+        }
+    }
+}
